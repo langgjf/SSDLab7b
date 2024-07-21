24a668f6ce8fb5caf623ae9f3f1487ce972faaf9
langgjf <113670444+langgjf@users.noreply.github.com>
2024-07-22 01:21:09 +0800

    Amended permission for deploy.sh and kill.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\langj\\OneDrive\\Documents\\Y2\\Y2T3\\ICT2216SSD\\Lab\\Labs\\jenkins-php-selenium-test\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
